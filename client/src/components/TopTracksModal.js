46cd65f258fd9e6adc2919f7c83a9e4dcb114534
ryanweiler92 <ryanweiler92@gmail.com>
2022-05-21 19:09:34 -0400

    display top tracks in modal

--- client/src/components/TopTracksModal.js
+++ client/src/components/TopTracksModal.js
@@ -7,18 +7,28 @@ const TopTracksModal = ({topTracks}) => {
         console.log(topTracks)
     }
 
-    return (
+   
 
+    return (
+        <>
             <Modal.Header className="d-flex align-items-center justify-content-center text-center" closeButton>
                 <Modal.Title className="d-flex align-items-center justify-content-center text-center">
-                    <h2>Artist</h2>
+                    <h2>Top Tracks for {}</h2>
                 </Modal.Title>
                 <Button onClick={myFunction}>ButtonMan</Button>
-                <Row>
-                {/* <iframe src="https://open.spotify.com/embed/track/6I5zXzSDByTEmYZ7ePVQeB?utm_source=generator" 
-                width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe> */}
-                </Row>
             </Modal.Header>
+            <Modal.Body>
+                {topTracks?.slice(0, 3).map((track) => {
+                    return (
+                        <Row className="centered-row mt-3">
+                            <iframe src={`https://open.spotify.com/embed/track/${track.id}?utm_source=generator`} width="100%" height="380" 
+                            frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                        </Row>
+                    )
+                })}
+
+            </Modal.Body>
+        </>
     )
 };
 
