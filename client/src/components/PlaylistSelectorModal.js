5eb8804362bcd515a6330fc469b470f28442594b
ryanweiler92 <ryanweiler92@gmail.com>
2022-05-22 09:41:04 -0400

    styling playlist selector modal

--- client/src/components/PlaylistSelectorModal.js
+++ client/src/components/PlaylistSelectorModal.js
@@ -1,29 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Nav, Navbar, Modal, Tab, Button, Row} from 'react-bootstrap'
+import {Container, Nav, Navbar, Modal, Tab, Button, Row, Col} from 'react-bootstrap'
 import background from '../assets/images/concert.jpg'
 
-const PlaylistSelectorModal = () => {
+const PlaylistSelectorModal = ({playlistData}) => {
 
-    const [showPlaylistModal, setShowPlaylistModal] = useState(false);
+    //playlists with 20 or more tracks
+    const qualifiedPlaylists = playlistData.filter(playlist => playlist.numberSongs >= 20)
 
+    // const modalStyle = {
+    //     backgroundImage: "url(" + background + ")",
+    //     backgroundSize: "cover",
+    //     backgroundPosition: "center"
+    // }
 
-    const modalStyle = {
-        backgroundImage: "url(" + background + ")",
-        backgroundSize: "cover",
-        backgroundPosition: "center"
+    const myFunction = () => {
+        console.log(playlistData)
+        console.log(qualifiedPlaylists)
     }
 
     return (
         <>
-            <Modal.Header className="d-flex align-items-center justify-content-center text-center" closeButton>
+            <Modal.Header className="d-flex align-items-center justify-content-center text-center dark-modal" closeButton>
                 <Modal.Title className="d-flex align-items-center justify-content-center text-center">
                     <h2>Select a Playlist</h2>
+                    <Button onClick={myFunction}>ButtonMan</Button>
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body style={modalStyle}>
+            <Modal.Body className="dark-modal m-auto">
+                <Row>
                 <p>Please select one of your playlists to use in the game. Only playlists
-                    with 10 or more tracks qualify.
+                    with 20 or more tracks qualify.
                 </p>
+                </Row>
+                {qualifiedPlaylists?.map((playlist) => {
+                    return (
+                        <Row className="centered-row mt-2 m-auto playlist-selector-items">
+                            <Col>
+                            <img 
+                            src={playlist.images[0].url}
+                            className="playlist-selector-img" />
+                            </Col>
+                            <Col>
+                            <p className="text-center">{playlist.name}</p>
+                            </Col>
+                            <Col>
+                            <p className="text-center">Songs: {playlist.numberSongs}</p>
+                            </Col>
+                            <Col>
+                            <Button value={playlist.id}>
+                                Select playlist
+                            </Button>
+                            </Col>
+                        </Row> 
+                    )
+                })}
             </Modal.Body>
         </>
     )
